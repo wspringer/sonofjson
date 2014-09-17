f93741600deaf2a551df077b76c902885ab5f2bc
Wilfred Springer <wilfredspringer@gmail.com>
2014-09-17 21:30:57 +0200

    Add the start of a builder.

--- src/main/scala/nl/typeset/sonofjson/Json.scala
+++ src/main/scala/nl/typeset/sonofjson/Json.scala
@@ -8,24 +8,30 @@ import scala.language.dynamics
 import scala.reflect.Manifest
 
 sealed abstract class Ref {
-  def resolve(name: String) = Named(this, name) 
+  def resolve(name: String) = Named(this, name)
+
   def resolve(index: Int) = Positional(this, index)
+
   def path: Option[String]
 }
+
 sealed abstract class SubRef[T <: Ref] extends Ref {
   def parent: T
 }
+
 case object Identity extends Ref {
   def path = None
 }
+
 case class Positional[T <: Ref](val parent: T, index: Int) extends SubRef[T] {
   override val path = Some(parent.path.getOrElse("") + "[" + index + "]")
 }
+
 case class Named[T <: Ref](val parent: T, name: String) extends SubRef[T] {
   override val path = Some(parent.path.getOrElse(".") + name)
 }
 
-class Json(value: JValue, ref: Ref = Identity) extends Dynamic with DefaultFormats {
+class Json(private [Json] val value: JValue, ref: Ref = Identity) extends Dynamic with DefaultFormats {
 
   private val formats = DefaultFormats
 
@@ -64,6 +70,27 @@ class Json(value: JValue, ref: Ref = Identity) extends Dynamic with DefaultForma
 
 object Json {
   def parse(str: String) = Json(JsonMethods.parse(str))
+
   def apply(value: JValue) = new Json(value)
+
+  object json extends Dynamic with Implicits with BigDecimalMode {
+
+    def applyDynamicNamed(method: String)(args: (String, Any)*) = method match {
+      case "obj" =>
+        Json(JObject(for ((name, value) <- args.toList) yield JField(name, value match {
+          case i: Int => JInt(i)
+          case i: Long => JInt(i)
+          case d: Double => JDouble(d)
+          case d: Float => JDouble(d)
+          case s: String => JString(s)
+          case b: Boolean => JBool(b)
+          case obj: Json => obj.value
+          case other => throw NotSupportedException("Missing support for " + other.getClass)
+        })))
+    }
+
+  }
+
 }
 
+case class NotSupportedException(msg: String) extends Exception(msg)
--- src/test/scala/nl/typeset/sonofjson/JsonSpec.scala
+++ src/test/scala/nl/typeset/sonofjson/JsonSpec.scala
@@ -6,7 +6,7 @@ case class Name(first: String, last: String)
 
 class JsonSpec extends Specification {
 
-  val json = Json.parse(
+  val parsed = Json.parse(
     """
       |{ "name": { "first": "wilfred", "last": "springer" }, "numbers": [9, 8, 7, 6] }
     """.stripMargin.trim
@@ -15,9 +15,29 @@ class JsonSpec extends Specification {
   "Json" should {
 
     "allow you to access data in a sensible way" in {
-      json.name.first.as[String] must be equalTo("wilfred")
-      json.numbers(0).as[Int] must be equalTo(9)
-      json.name.as[Name] must be equalTo(Name("wilfred", "springer"))
+      parsed.name.first.as[String] must be equalTo("wilfred")
+      parsed.numbers(0).as[Int] must be equalTo(9)
+      parsed.name.as[Name] must be equalTo(Name("wilfred", "springer"))
+    }
+
+    "allow you to build JSON objects the easy way" in {
+      import Json.json
+      val obj = json.obj(
+        first = "wilfred",
+        last = "springer",
+        age = 41,
+        scala = true,
+        address = json.obj(
+          street = "Zuiderdiep 32",
+          city = "Groningen"
+        )
+      )
+      obj.first.as[String] must be equalTo("wilfred")
+      obj.last.as[String] must be equalTo("springer")
+      obj.age.as[Int] must be equalTo(41)
+      obj.scala.as[Boolean] must be equalTo(true)
+      obj.address.city.as[String] must be equalTo("Groningen")
+      ok
     }
 
   }
