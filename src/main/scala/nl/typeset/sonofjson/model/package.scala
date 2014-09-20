7b337e11d56055b87d558e707990ef4ffad1e9bf
Wilfred Springer <wilfredspringer@gmail.com>
2014-09-20 17:56:20 +0200

    More tests, extractors and accessors.

--- src/main/scala/nl/typeset/sonofjson/model/package.scala
+++ src/main/scala/nl/typeset/sonofjson/model/package.scala
@@ -5,16 +5,48 @@ import java.io.Reader
 import scala.collection.mutable
 import scala.util.parsing.combinator.JavaTokenParsers
 import scala.util.parsing.input.Position
+import scala.reflect.runtime.universe._
+import scala.language.dynamics
 
 package object model {
 
-  sealed trait JValue
+  type Extractor[T] = PartialFunction[JValue, T]
+  implicit val IntExtractor: Extractor[Int] = { case JNumber(number) => number.toInt }
+  implicit val DoubleExtractor: Extractor[Double] = { case JNumber(number) => number.toDouble }
+  implicit val FloatExtractor: Extractor[Float] = { case JNumber(number) => number.toFloat }
+  implicit val LongExtractor: Extractor[Long] = { case JNumber(number) => number.toLong }
+  implicit val StringExtractor: Extractor[String] = {
+    case JString(str) => str
+    case JNumber(number) => number.toString()
+    case JNull => "null"
+    case JUndefined => "#undefined"
+    case JBool(bool) => bool.toString
+  }
+  implicit val BoolExtractor: Extractor[Boolean] = { case JBool(bool) => bool }
+  implicit val BigDecimalExtractor: Extractor[BigDecimal] = { case JNumber(number) => number }
+  implicit def jvalueTo[T](value: JValue)(implicit extract: Extractor[T], tag: TypeTag[T]): T =
+    value.as[T](extract, tag)
+
+  sealed abstract class JValue extends Dynamic {
+
+    def selectDynamic(name: String): JValue = this match {
+      case JObject(elements) => elements.getOrElse(name, JUndefined)
+      case _ => throw NotSupportedException(s"""$toString does not have an attribute called "$name"""")
+    }
+
+    def as[T](implicit extract: Extractor[T], tag: TypeTag[T]) =
+      extract.applyOrElse(this, { _: JValue =>
+        throw NotSupportedException(s"Missing support for extracting ${tag.tpe.typeSymbol.asClass.fullName}} from $this")
+      })
+
+  }
   case class JObject(elements: mutable.Map[String, JValue]) extends JValue
   case class JArray(elements: mutable.Buffer[JValue]) extends JValue
   case class JNumber(value: BigDecimal) extends JValue
   case class JBool(value: Boolean) extends JValue
   case class JString(value: String) extends JValue
   case object JNull extends JValue
+  case object JUndefined extends JValue
 
   def parse(str: String) = Parser.parse(str)
   def parse(reader: Reader) = Parser.parse(reader)
@@ -54,5 +86,7 @@ package object model {
   }
 
   case class ParserException(msg: String, pos: Position) extends Exception(msg)
+  case class NotSupportedException(msg: String) extends Exception(msg)
+
 
 }
---
+++ src/test/scala/nl/typeset/sonofjson/model/AccessorSpec.scala
@@ -0,0 +1,46 @@
+package nl.typeset.sonofjson.model
+
+import org.specs2.mutable.Specification
+
+class AccessorSpec extends Specification {
+
+  val json = parse(
+    """
+      |{ "name" : { "first" : "John", "last" : "Doe" }, "age": 41, "married": true }
+    """.stripMargin.trim)
+
+  "A JObject" should {
+
+    "allow you to access properties by name" in {
+      json.name.first.as[String] must be equalTo("John")
+    }
+
+    "consider undefined properties to be of type JUndefined" in {
+      json.employee must be equalTo(JUndefined)
+    }
+
+    "throw an exception when you try to resolve an attribute on something that doesn't have any" in {
+      json.employee.name must throwA[NotSupportedException]
+    }
+
+    "allow you to access numeric properties as Int" in {
+      json.age.as[Int] must be equalTo(41)
+    }
+
+    "allow you to access boolean properties as Boolean" in {
+      json.married.as[Boolean] must beTrue
+    }
+
+    "allow you to access BigDecimal properties as… BigDecimal" in {
+      json.age.as[BigDecimal] must be equalTo(BigDecimal(41))
+    }
+
+    "leave type out in case there's no doubt" in {
+      // IntelliJ is confused here, but Scala isn't
+      val age: Int = json.age
+      age must be equalTo(41)
+    }
+
+  }
+
+}
